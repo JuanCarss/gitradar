272a328bd54394fce850759b79869d8497d4284e
juancarss <juancarss2002@gmail.com>
2024-01-29 14:14:39 +0000

    Service Implemented

--- metrics_provider/src/main/java/es/ulpgc/Service.java
+++ metrics_provider/src/main/java/es/ulpgc/Service.java
@@ -4,33 +4,54 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import es.ulpgc.aws.s3.S3;
 import es.ulpgc.aws.s3.S3ClientBuilder;
 import es.ulpgc.readers.S3Reader;
 import software.amazon.awssdk.services.s3.S3Client;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Map;
 
 public class Service implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
     @Override
     public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        APIGatewayProxyResponseEvent responseEvent = new APIGatewayProxyResponseEvent();
         Map<String, String> params = input.getQueryStringParameters();
         S3Client s3Client = new S3ClientBuilder().withEndpoint(System.getenv("CUSTOM_ENDPOINT_URL")).at(System.getenv("REGION")).build();
-        String response = new S3Reader().read(S3.getObjectFrom(s3Client, System.getenv("METRICS_BUCKET_ID"), params.get("filename")));
-        APIGatewayProxyResponseEvent responseEvent = new APIGatewayProxyResponseEvent();
-        responseEvent.setBody(response);
+        if (!metricsExists(s3Client, params.get("filename"))) {
+            responseEvent.setBody("File does not exist.");
+            responseEvent.setStatusCode(404);
+            return responseEvent;
+        }
+        String metrics = getFileMetrics(s3Client, params.get("filename"));
+        if (params.containsKey("class")) metrics = getClassMetrics(metrics, params.get("class"));
+        if (metrics.isEmpty()) {
+            responseEvent.setBody("Class does not exist in file.");
+            responseEvent.setStatusCode(404);
+            return responseEvent;
+        }
+        responseEvent.setBody(metrics);
         responseEvent.setStatusCode(200);
         return responseEvent;
     }
 
-    private String read(InputStream is) {
-        try (BufferedInputStream bis = new BufferedInputStream(is)) {
-            return new String(bis.readAllBytes());
-        } catch (IOException e) {
-            return "{}";
+    private String getClassMetrics(String metrics, String className) {
+        JsonArray classes = JsonParser.parseString(metrics).getAsJsonObject().get("classes").getAsJsonArray();
+        for (int i = 0; i < classes.size(); i++) {
+            JsonObject classObject = classes.get(i).getAsJsonObject();
+            if (classObject.get("identifier").getAsString().equals(className)) return classObject.toString();
         }
+        return "";
+    }
+
+    private static String getFileMetrics(S3Client s3Client, String filename) {
+        return new S3Reader().read(S3.getObjectFrom(s3Client, System.getenv("METRICS_BUCKET_ID"), filename));
+    }
+
+    private static boolean metricsExists(S3Client s3Client, String filename) {
+        return S3.keysIn(s3Client, System.getenv("METRICS_BUCKET_ID"), "")
+                .anyMatch(s -> s.equals(filename));
     }
 }
--- metrics_provider/src/main/java/es/ulpgc/aws/s3/S3.java
+++ metrics_provider/src/main/java/es/ulpgc/aws/s3/S3.java
@@ -2,11 +2,17 @@ package es.ulpgc.aws.s3;
 
 import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.*;
+
+import java.util.stream.Stream;
 
 public class S3 {
     public static ResponseInputStream<GetObjectResponse> getObjectFrom(S3Client client, String bucketName, String keyName) {
         return client.getObject(GetObjectRequest.builder().bucket(bucketName).key(keyName).build());
+    }
+
+    public static Stream<String> keysIn(S3Client client, String bucketName, String prefix) {
+        return client.listObjects(ListObjectsRequest.builder().bucket(bucketName).prefix(prefix).build())
+                .contents().stream().map(S3Object::key);
     }
 }
